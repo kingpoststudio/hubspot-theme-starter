be738cc22d2ec45b8f0c5e23f01807e315744ed7
Stuart Grant <stu@campgrantnc.com>
2023-04-24 21:28:58 -0400

    Create HeroBanner

--- lib/src/modules/hero-banner.ts
+++ lib/src/modules/hero-banner.ts
@@ -1,5 +1,29 @@
-const { LitElement, html, css, customElement, property } = window.Lit;
+const { LitElement, html, css, customElement } = window.Lit;
 
 @customElement('hubspot-hero-banner')
 export default class HeroBanner extends LitElement {
+  static styles = css`
+    .wrapper {
+      display: flex;
+      flex-direction: column;
+      max-width: var(--page-max-width);
+      margin: 0 auto;
+      padding: 2rem;
+      gap: 1rem;
+      background: var(--color-black);
+      color: var(--color-white);
+      text-align: center;
+      border-radius: 0 0 1rem 1rem;
+    }
+  `;
+
+  render() {
+    return html`
+    <div class="wrapper">
+      <slot name="title"></slot>
+      <slot name="content"></slot>
+      <slot name="cta"></slot>
+    </div>
+    `;
+  }
 }
