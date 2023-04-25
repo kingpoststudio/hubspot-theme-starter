bef5baf2d59c9c3e7fe3f514bc1ba045afb31715
Stuart Grant <stu@campgrantnc.com>
2023-04-24 21:29:22 -0400

    Add Grid component

--- lib/src/components/container.ts
+++ lib/src/components/container.ts
@@ -6,7 +6,7 @@ export default class Container extends LitElement {
   static styles = css`
     :host {
       display: block;
-      max-width: 1200px;
+      max-width: var(--page-max-width);
       margin: 0 auto;
       padding: 2rem;
     }
---
+++ lib/src/components/grid.ts
@@ -0,0 +1,20 @@
+const { LitElement, html, css, property, customElement } = window.Lit;
+
+@customElement('hubspot-grid')
+export default class Grid extends LitElement {
+  static styles = css`
+    :host {
+      display: grid;
+      grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+      gap: 1rem;
+      margin: 0 auto;
+      padding: 2rem;
+    }
+  `;
+
+  render() {
+    return html`
+      <slot></slot>
+    `;
+  }
+}
