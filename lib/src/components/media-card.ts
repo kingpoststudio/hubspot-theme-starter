e6361ce3883b55e49156a61ec164094bae5b6691
Shanie <shanieportal@gmail.com>
2023-10-29 05:05:04 -0400

    Styled Media Card

--- lib/src/components/media-card.ts
+++ lib/src/components/media-card.ts
@@ -4,21 +4,73 @@ const { LitElement, html, css } = window.Lit;
 
 class MediaCard extends LitElement {
     static styles = css`
+        
         .wrapper {
-            width: 10rem;
-            background: purple;
-            color: black;
-            border: 0.25rem solid green;
+            display: flex;
+            justify-content: center;
+            flex-wrap: wrap;
+            margin-top: 6.25rem;
         }
+
+        .container {
+            background: white;
+            width: 19.18rem;
+            height: 24rem;
+            margin: 1rem;
+            border: 0.25rem solid black;
+            border-radius: 1rem;
+        }
+
+        .container-image {
+            height: 12rem;
+            margin-bottom: 1rem;
+            background-size: cover;
+            border-radius: 0.75rem 0.75rem 0 0;
+        }
+
+        .container-content {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: space-between;
+            padding: 0rem 0.5rem;
+        }
+
+        ::slotted([slot="title"]) {
+            text-transform: uppercase;
+        }
+
+        ::slotted([slot="description"]) {
+            font-size: 14px;
+            line-height: 1;
+            color: black;
+            text-align: center;
+        }
+
+        ::slotted([slot="button"]) {
+            background-color: black;
+            color: white;
+            padding: 0.625rem 1.25rem;
+            border: none;
+            border-radius: 0.5rem;
+            cursor: pointer;
+            margin-top: 0.5rem;
+        }
+
     `;
 
     render() {
-        console.log(this.children);
         return html`
+        
         <div class="wrapper">
-            <slot name="image"></slot>
-            <slot name="title"></slot>
-            <slot name="button"></slot>
+            <div class="container">
+                <div class="container-image" style="background-image: url('https://picsum.photos/300/200');"> 
+            </div>
+            <div class="container-content">
+                <slot name="title"></slot>
+                <slot name="description"></slot>
+                <slot name="button"></slot>
+            </div>
         </div>
         `;
     }
--- lib/src/stories/media-card.stories.ts
+++ lib/src/stories/media-card.stories.ts
@@ -13,9 +13,15 @@ export default {
 export const Base = () => {
   return html`
     <hubspot-media-card>
-      <img src="https://picsum.photos/200/300" slot="image" />
-      <h3 slot="title"> Title </h3>
-      <button slot="button"> Button </button>
+      <h3 slot="title"> Hello World of Images </h3>
+      <p slot="description"> 
+      Welcome to a world of captivating visuals where images tell 
+      stories. Explore a diverse collection of images that traverse landscapes, cultures, and 
+      imaginations. Whether you're a coding enthusiast or simply an 
+      admirer of the pixelated, this is your 'Hello World' to stunning 
+      visuals.
+      </p>
+      <button slot="button"> Explore! </button>
     </hubspot-media-card>
   `;
 };
