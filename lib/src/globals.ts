4cc7e2b6f4b14b522c406ea2927a72c9141623cc
Stuart Grant <stu@campgrantnc.com>
2024-01-16 14:57:02 -0500

    Well sir, it's kaput

--- lib/src/globals.ts
+++ lib/src/globals.ts
@@ -1,16 +1,1 @@
-import { LitElement, html, css } from 'lit';
-import { customElement, property } from 'lit/decorators.js';
-
-declare global {
-  interface Window {
-    Lit: {
-      LitElement: typeof LitElement;
-      html: typeof html;
-      css: typeof css;
-      customElement: typeof customElement;
-      property: typeof property;
-    };
-  }
-}
-
-window.Lit = { LitElement, html, css, customElement, property };
+import '@kingpoststudio/truss-ui/dist/globals.js';
